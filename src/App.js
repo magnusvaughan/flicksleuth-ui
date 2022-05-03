006712e1898378ec3d58e721c05c8e32d6f1832c
mvaughan <magnus.vaughan@gmail.com>
2022-05-03 23:08:38 +0100

    Fixed revealClue call

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ function App() {
                       disabled={guess === ""}
                     />
                     <button
-                      onClick={() => revealClue}
+                      onClick={revealClue}
                       type="button"
                       className="inline-flex m-1 items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
